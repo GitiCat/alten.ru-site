1e555b25598aaceaca66555ac82bf6870c9ae04d
Zhuravlev Ivan <ivan_zhuravlev_1997@mail.ru>
2020-06-14 11:18:05 +0300

    add tslint to project

--- src/app/components/pages/home/elements/product/element.tsx
+++ src/app/components/pages/home/elements/product/element.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 type ProductBlockTypes = {
-    title: string,
-    image: string,
-    url: string
-}
+    title: string;
+    image: string;
+    url: string;
+};
 
 const imageUrl = (imageName: string) => {
-    return `/images/${imageName}`
-}
+    return `/images/${imageName}`;
+};
 
 const ProductBlock: React.FunctionComponent<ProductBlockTypes> = (props) => {
     return (
         <div className="element">
-            <div className="bg" style={{backgroundImage: 'url("' + imageUrl(props.image) + '")'}}></div>
+            <div
+                className="bg"
+                style={{
+                    backgroundImage: 'url("' + imageUrl(props.image) + '")',
+                }}
+            ></div>
             <div className="context">
                 <span>{props.title}</span>
                 <Link to={props.url}>Перейти</Link>
             </div>
+            <div className="hover-bl">
+                
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ProductBlock;
