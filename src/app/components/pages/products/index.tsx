a8aff943ea0fc6f5a6951e3a2814e13d455c97e8
Zhuravlev Ivan <ivan_zhuravlev_1997@mail.ru>
2020-08-07 16:46:19 +0300

    Change footer, feedback, news in home styles

--- src/app/components/pages/products/index.tsx
+++ src/app/components/pages/products/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react'
+import Header from '../../blocks/header/header'
+import { getAsyncData } from '../../../utils/async-get-data'
+import { asyncDataReducer ,initialState } from '../../../utils/async-data-states/reducer'
+import { FETCHED, ERROR } from '../../../utils/async-data-states/types'
 
 const ProductsComponent: React.FunctionComponent = () => {
+    const [state, dispatch] = useReducer(asyncDataReducer, initialState)
+
+    useEffect(() => {
+        getAsyncData({
+            api_v: 0,
+            url: 'products',
+            params: {}
+        })
+        .then(result => dispatch({ type: FETCHED, payload: { data: result }}))
+        .catch(error => dispatch({ type: ERROR, payload: { errorString: error }}))
+    }, [])
+    
     return (
-        <div></div>
+        <div>
+            {!state.loading &&
+                <React.Fragment>
+                    <Header title='Продукция' subtitle='Продукция нашего предприятия'/>
+                    <div className="article">
+                        
+                    </div>
+                </React.Fragment>
+            }
+        </div>
     )
 }
 
